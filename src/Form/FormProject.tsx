91323c3e0f999673bb4e8e7106fee41fa3a38544
Ivan Kovacevic <ivanbkovacevic@gmail.com>
2024-01-10 12:41:22 +0100

    rr

--- src/Form/FormProject.tsx
+++ src/Form/FormProject.tsx
@@ -13,7 +13,7 @@ interface FormProps {
 }
 
 const FormProject: React.FC<FormProps> = ({ formAction }) => {
-  const { state, addNewProject, editProject, selectProject } =
+  const { state, addNewProject, editProject } =
     useContext(Context);
   const { selectedProject } = state;
   const { togglePopUp, stateUI } = useContext(ContextUI);
